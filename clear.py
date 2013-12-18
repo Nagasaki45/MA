a2ef5c6f360038dfce530f15aaf67848f82c4a72
Tom Gurion <nagasaki45@gmail.com>
2013-12-18 14:58:30 +0200

    Update clear.py to be standalone

--- clear.py
+++ clear.py
@@ -1,38 +1,26 @@
 #!/usr/bin/env python
 
 '''
-Clears the directory according to '.gitignore'
+Clears the directory.
 '''
 
 import os
 
-TO_EXCEPT = [
-    '.git',
-    '.pdf',
+TO_REMOVE = [
+    '.aux',
+    '.fdb_latexmk',
+    '.log',
+    '.out',
+    '.gz',
 ]
 
 
 def main():
 
-    # read .gitignore
-    with open('.gitignore') as f:
-        lines = [l.strip('\n') for l in f.readlines()]
-
-    # add files to except from .gitignore
-    for line in lines:
-        if len(line) > 0 and line[0] == '!':
-            TO_EXCEPT.append(line.strip('!').strip('*'))
-
-    # remove the files
-    for file in os.listdir('.'):
-        to_remove = True
-        for e in TO_EXCEPT:
-            if file.endswith(e):
-                to_remove = False
-                break
-
-        if to_remove:
-            os.remove(file)
+    for f in os.listdir('.'):
+        extention = os.path.splitext(f)[1]
+        if extention in TO_REMOVE:
+            os.remove(f)
 
 
 if __name__ == '__main__':
